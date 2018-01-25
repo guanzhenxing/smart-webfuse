902dd6437f10f1fc5a633e9c92828e72dd79a1a3
guanzhenxing <guan.zhenxing@foxmail.com>
2018-01-26 07:52:56 +0800

    学习java8-2

--- src/test/java/net/webfuse/Java8Demo.java
+++ src/test/java/net/webfuse/Java8Demo.java
@@ -1,8 +1,12 @@
 package net.webfuse;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Java8Demo {
 
@@ -27,6 +31,28 @@ public class Java8Demo {
         Arrays.asList(1, 2).stream().map(Math::abs).collect(Collectors.toList());//情况2：类::静态方法
         Arrays.sort(new String[]{"", ""}, Comparator.comparingInt(String::length)); //情况3：类::实例方法。第一个参数会成为执行方法的对象.String::length 等于 (x)->x.length()
         String[] arr = Arrays.asList("", "").stream().toArray(String[]::new);   //情况4：类::new
+
+    }
+
+
+    public void aboutStream() {
+        List<String> words = new ArrayList<>();
+        long count = words.parallelStream().filter(w -> w.length() > 12).count();
+
+        //Stream遵循“做什么，而不是怎么去做”的原则。
+        //在使用Stream时候，你会通过三个阶段来建立一个操作流水线：
+        //1-创建一个Stream
+        //2-在一个或多个步骤中，指定将初始Stream转换为另一个Stream的中间操作
+        //3-使用一个终止操作来产生一个结果。该操作会强制它之前的延迟操作立即执行。在这之后，该Stream操作就不会再被使用了。
+
+
+        Stream<String> wordStream = Stream.of(new String[]{});
+        Stream<String> song = Stream.of("","","");
+
+        Stream<String> echos = Stream.generate(()->"Echo"); //创建一个含有常亮值的Stream
+        Stream<Double> randoms = Stream.generate(Math::random); //创建一个含有随机数的Stream
+        //iterate方法接受一个“种子”的值和一个函数作为参数
+        Stream<BigInteger> integers = Stream.iterate(BigInteger.ZERO,n ->n.add(BigInteger.ONE));
 
     }
 
