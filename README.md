6624ca3ea4435bde3dc429073d3b4350148357ba
Jesen <guan.zhenxing@foxmail.com>
2018-08-03 18:03:44 +0800

    一些注释

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # smart-webfuse
 
 smart-webfuse是一个基于SpringBoot封装的快速开发框架。
+
+
+## TODO LIST
+
+- 异常处理
+    - 校验异常的时候，返回错误信息（参考：https://github.com/jirutka/spring-rest-exception-handler）
+    - 思考是否可以在异常上定义注解，然后返回值根据注解的信息返回（参考：https://github.com/raychatter/spring-restful-exception-handler）
--- src/main/java/cn/webfuse/framework/exception/NotFoundBizException.java
+++ src/main/java/cn/webfuse/framework/exception/NotFoundBizException.java
@@ -1,6 +1,10 @@
 package cn.webfuse.framework.exception;
 
+/**
+ * 没有发现资源异常
+ */
 public class NotFoundBizException extends AbstractBizException {
+
     public NotFoundBizException(int status, String errorCode, String message, Throwable throwable, String developerMessage) {
         super(status, errorCode, message, throwable, developerMessage);
     }
