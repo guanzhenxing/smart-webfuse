2568110f750a623b99ddd1e6828739bd152ceed1
Jesen <guan.zhenxing@foxmail.com>
2019-02-27 21:05:24 +0800

    init

---
+++ smart-webfuse-cloud/smart-webfuse-cloud-service-clients/smart-webfuse-cloud-service-client/src/main/java/cn/webfuse/framework/Application.java
@@ -0,0 +1,15 @@
+package cn.webfuse.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringCloudApplication
+@EnableFeignClients
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
