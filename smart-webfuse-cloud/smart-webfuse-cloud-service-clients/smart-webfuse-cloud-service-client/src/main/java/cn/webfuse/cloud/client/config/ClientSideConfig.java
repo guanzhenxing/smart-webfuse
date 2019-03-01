c798118814f4150000eaec7afdd113a64827edce
Jesen <guan.zhenxing@foxmail.com>
2019-03-01 14:19:38 +0800

    for new

---
+++ smart-webfuse-cloud/smart-webfuse-cloud-service-clients/smart-webfuse-cloud-service-client/src/main/java/cn/webfuse/cloud/client/config/ClientSideConfig.java
@@ -0,0 +1,45 @@
+package cn.webfuse.cloud.client.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 客户端配置。配置Ribbon，Feign等
+ */
+@Configuration
+public class ClientSideConfig {
+    /**
+     * 负载均衡的RestTemplate。使用方法为：
+     * <p>
+     *
+     * <code>
+     * {@literal @}Autowired<br/>
+     * {@literal @}LoadBalanced<br/>
+     * private RestTemplate loadBalanced;
+     * </code>
+     */
+    @LoadBalanced
+    @Bean
+    RestTemplate loadBalanced() {
+        return new RestTemplate();
+    }
+
+    /**
+     * 常规的RestTemplate。使用方法为：
+     * <p>
+     *
+     * <code>
+     * {@literal @}Autowired<br/>
+     * private RestTemplate loadBalanced;
+     * </code>
+     */
+    @Primary
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
