810ff6eae0588f6e552d34d108b8e9301777dc67
Jesen <guan.zhenxing@foxmail.com>
2019-04-16 22:20:30 +0800

    for spring cloud

--- smart-webfuse-cloud/auth-service/src/main/java/cn/webfuse/cloud/auth/UaaApplication.java
+++ smart-webfuse-cloud/auth-service/src/main/java/cn/webfuse/cloud/auth/UaaApplication.java
@@ -2,9 +2,12 @@ package cn.webfuse.cloud.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringCloudApplication
+@EnableFeignClients
 @ComponentScan(basePackages = {"cn.webfuse"})
 public class UaaApplication {
 
